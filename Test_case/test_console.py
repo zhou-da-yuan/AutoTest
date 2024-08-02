cee787f6be9eb03200d2d28f6ff059e68d265a30
dayuan <2862840426@qq.com>
2024-08-02 14:32:04 +0800

    init

--- Test_case/test_console.py
+++ Test_case/test_console.py
@@ -1,15 +1,15 @@
 import requests
 import pytest
 from requests import session
-from com.read_con import read_json
+from com.read_inter import inter
 
-base_data = read_json(r'..\config\config.json')
-base_url = base_data['base_url']
-user = base_data['user']
-password = base_data['password']
+base_url = inter(181)['base_url']
+user = inter(181)['user']
+password = inter(181)['password']
 
 s = session()
 s.verify = False
+
 
 class TestConsole:
 
@@ -20,8 +20,6 @@ class TestConsole:
             data={"user": user, "password": password}
         )
         assert response.status_code == 200
-        print(response.cookies)
-        print(response.text)
 
     def test_search_user(self):
         response = s.request(
